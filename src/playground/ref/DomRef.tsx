dcb48c3f8e80bbe27b396c9a095d4d30aa6b577d
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-30 22:21:20 +0100

    display generics

--- src/playground/ref/DomRef.tsx
+++ src/playground/ref/DomRef.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react'
 import { ClassCounter } from '../class/ClassCounter'
 import Private from '../componentProp/Private'
 import { Profile } from '../componentProp/Profile'
+import List from '../generics/List'
 
 const DomRef = () => {
   const inputRef = useRef<HTMLInputElement>(null!)
@@ -15,13 +16,17 @@ const DomRef = () => {
     <div>
       <h2>domref</h2>
       <input type="text" ref={inputRef} />
-
       <h3>Class Counter</h3>
       <ClassCounter message="The counter value is " />
-
-      <h3>Auth</h3>
+      <h3 className="pt-3">Auth</h3>
       <Private isLoggedIn={false} component={Profile} />
       <Private isLoggedIn={true} component={Profile} />
+      <h3 className="pt-3">List</h3>
+      <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={item => console.log(item)}
+      />
+      <List items={[1, 2, 3, 4]} onClick={item => console.log(item)} />
     </div>
   )
 }
