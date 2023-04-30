192e17d7ce24414968ad5bb694570597751743bf
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-30 22:18:59 +0100

    added restrictions eg, commented

--- src/playground/generics/List.tsx
+++ src/playground/generics/List.tsx
@@ -5,7 +5,9 @@ type ListProps<T> = {
   onClick: (value: T) => void
 }
 
-const List = <T extends {}>({ items, onClick }: ListProps<T>) => {
+// extends {} means except anything
+// restrictions can be set eg, T extends number | string
+const List = <T extends number | string>({ items, onClick }: ListProps<T>) => {
   return (
     <div>
       <h2>List of items</h2>
