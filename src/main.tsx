6bb8e1955c7ef48fac2acd61ced232298fb1af40
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-30 16:20:30 +0100

    added store to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { store } from './app/store'
+import store from './app/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <Provider> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
