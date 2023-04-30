fe55de67627cfe874528028ef916331f8a2e7048
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-30 22:35:30 +0100

    restricting props

---
+++ src/playground/restriction/RandomNumber.tsx
@@ -0,0 +1,45 @@
+type RandomNumberType = {
+  value: number
+  isPositive?: boolean
+  isNegative?: boolean
+  isZero?: boolean
+}
+
+type PositiveNumber = RandomNumberType & {
+  isPositive: boolean
+  isNegative?: never
+  isZero?: never
+}
+
+type NegativeNumber = RandomNumberType & {
+  isNegative: boolean
+  isPositive?: never
+  isZero?: never
+}
+
+type Zero = RandomNumberType & {
+  isZero: boolean
+  isPositive?: never
+  isNegative?: never
+}
+
+type RandomNumberProps = PositiveNumber | NegativeNumber | Zero
+
+const RandomNumber = ({
+  value,
+  isPositive,
+  isNegative,
+  isZero,
+}: RandomNumberProps) => {
+  return (
+    <div>
+      <h3>RandomNumber</h3>
+      {value} {isPositive && 'positive'} {isNegative && 'negative'} {''}
+      {isZero && 'zero'}
+    </div>
+  )
+}
+
+export default RandomNumber
+
+// imported in /ref
