9a3c0bada739e4ddc89df088141fb4fba0b875f5
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-30 21:53:21 +0100

    display of component prop

--- src/playground/ref/DomRef.tsx
+++ src/playground/ref/DomRef.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react'
 import { ClassCounter } from '../class/ClassCounter'
+import Private from '../componentProp/Private'
+import { Profile } from '../componentProp/Profile'
 
 const DomRef = () => {
   const inputRef = useRef<HTMLInputElement>(null!)
@@ -16,6 +18,10 @@ const DomRef = () => {
 
       <h3>Class Counter</h3>
       <ClassCounter message="The counter value is " />
+
+      <h3>Auth</h3>
+      <Private isLoggedIn={false} component={Profile} />
+      <Private isLoggedIn={true} component={Profile} />
     </div>
   )
 }
