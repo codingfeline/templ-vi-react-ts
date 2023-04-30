979dc7499209087d7492c5abcc315763922b21a3
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-30 16:16:56 +0100

    typescript for store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,9 +2,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import authSlice from '../features/auth/authSlice'
 import counterSlice from '../features/counter/counterSlice'
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     auth: authSlice,
     counter: counterSlice,
   },
 })
+
+export default store
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
